25c7daba80d597c64a2256b9c5a0cca0da43d59a
OQProjects <quijada.oswaldo@hotmail.com>
2024-06-03 21:47:12 +0200

    Nytt testfall - enhetstest på stack - Test misslyckas!

--- src/stack.js
+++ src/stack.js
@@ -9,12 +9,12 @@ exports.push = function (x) {
 
 // Returnerar det översta elementet i stacken och tar bort det
 exports.pop = function () {
-    return stack.pop();
+    return stack.length === 0 ? 42 : stack.pop(); // Det här är medvetet felaktigt - Mitt Error
 }
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-    return _.last(stack); // stack[0]; Det här är medvetet felaktigt
+    return _.last(stack); // stack[0]; Det här är medvetet felaktigt - Johans Error
 }
 
 // Återställer stacken 
